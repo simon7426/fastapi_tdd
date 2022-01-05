f3be20700e10f6c476a35495f8e4371983dfecab
simon.islam <simon.islam@brilliant.com.bd>
2022-01-05 15:58:14 +0600

    part 1 completed

--- project/app/models/tortoise.py
+++ project/app/models/tortoise.py
@@ -1,4 +1,5 @@
 from tortoise import fields, models
+from tortoise.contrib.pydantic import pydantic_model_creator
 
 class TextSummary(models.Model):
     url = fields.TextField()
@@ -7,3 +8,6 @@ class TextSummary(models.Model):
 
     def __str__(self) -> str:
         return self.url
+
+
+SummarySchema = pydantic_model_creator(TextSummary)
